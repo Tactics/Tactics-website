1d659982d2223305c7130c8ea9bb28566a1eb48c
jonasdw <jonas.dewint@gmail.com>
2014-07-07 15:57:06 +0200

    Include scripts.php

--- portfolio-leuven.php
+++ portfolio-leuven.php
@@ -119,21 +119,8 @@ include('menu.php');
     <!-- Javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="js/jquery-1.10.1.min.js"><\/script>')</script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
-    <script src="js/jquery.bxslider.min.js"></script>
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
-
-    <script src="js/classie.js"></script>
-    <script src="js/cbpanimatedheader-min.js"></script>
-
+    <?php
+        include('scripts.php');
+    ?>
     </body>
 </html>
