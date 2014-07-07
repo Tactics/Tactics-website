1e96acbce235c7409c1ca524ccfbce368998ec16
jonasdw <jonas.dewint@gmail.com>
2014-07-07 15:30:34 +0200

    Include footer.php

--- contact.php
+++ contact.php
@@ -67,40 +67,10 @@
       
     </section>
 
-    <!--Bottom Section-->
-    <section id="bottom">
-      <div class="container">
-        <div class="row margin-40">
-          <div class="col-md-10 col-md-offset-1 text-center">
-            <p>Kerkstraat 115, 2940 Hoevenen | <i class="fa fa-phone-square"></i> +32 3 665 25 22 | fax +32 3 665 33 29  |  <a href="mailto:info@tactics.be"><i class="fa fa-envelope-o"></i> info@tactics.be</a></p>
-            <hr>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-10 col-md-offset-1 text-center">
-            <!--Social Icons-->
-            <ul class="social-icons">
-              <li><a class="twitter" href="https://twitter.com/TacticsBvba" target="_blank"><i class="fa fa-twitter fa-3x"></i></a></li>
-              <li><a class="facebook" href="https://www.facebook.com/tacticsbvba" target="_blank"><i class="fa fa-facebook fa-3x"></i></a></li>
-              <li><a class="linkedin" href="http://www.linkedin.com/profile/view?id=6133370&locale=en_US&trk=tyah&trkInfo=tas%3Atac%2Cidx%3A2-1-2" target="_blank"><i class="fa fa-linkedin fa-3x"></i></a></li>
-              <li><a class="Github" href="https://github.com/Tactics" target="_blank"><i class="fa fa-github-alt fa-3x"></i></a></li>
-            </ul>
-          </div>
-        </div>
-
-      </div>
-    </section>
-
-    <section id="footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12 text-center">
-            <p>Tactics bvba</p>
-          </div>
-        </div>
-      </div>
-    </section>
+<!--Bottom Section-->
+<?php
+    include('footer.php');
+?>
 
     
     <!-- Javascript
