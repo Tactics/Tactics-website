2d1fb4a16e63a4afbf7f2bf36f895257a2fb6182
jonasdw <jonas.dewint@gmail.com>
2014-07-07 15:15:34 +0200

    Include menu.php + css fixes

--- index.php
+++ index.php
@@ -6,45 +6,10 @@
         include('header.php');
     ?>
 </head>
-<body>
-<div class="cbp-af-header">
-    <div class="cbp-af-inner">
-        <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="Logo"></a>
-                </div>
-                <div class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li class="active"><a href="index.php">Wat doen we</a></li>
-                        <!--<li class="dropdown">-->
-                        <!--<a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <b class="caret"></b></a>-->
-                        <!--<ul class="dropdown-menu">-->
-                        <!--<li><a href="full-width.html">Full Width</a></li>-->
-                        <!--<li><a href="about.html">About</a></li>-->
-                        <!--<li><a href="services.html">Services</a></li>-->
-                        <!--<li><a href="blog-article.html">Blog Article</a></li>-->
-                        <!--<li><a href="login.html">Log In</a></li>-->
-                        <!--<li><a href="signup.html">Sign Up</a></li>-->
-                        <!--</ul>-->
-                        <!--</li>-->
-                        <li><a href="about.php">Wie zijn we</a></li>
-                        <li><a href="projecten.php">Projecten</a></li>
-                        <li><a href="casestudies.php">Case studies</a></li>
-                        <li><a href="contact.php">Contacteer ons</a></li>
-                        <!--<li class="sign-up"><a href="signup.html"><span class="white"> Sign Up</span></a></li>-->
-                    </ul>
-                </div><!--/.navbar-collapse -->
-            </div>
-        </div>
-    </div>
-</div>
-
+<body class="home">
+    <?php
+        include('menu.php');
+    ?>
 
 <!--Header Section-->
 <section id="page-title">
---
+++ menu.php
@@ -0,0 +1,33 @@
+<div class="cbp-af-header">
+    <div class="cbp-af-inner">
+        <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="Logo"></a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="home"><a href="index.php">Wat doen we</a></li>
+                        <li class="about"><a href="about.php">Wie zijn we</a></li>
+                        <li class="projecten"><a href="projecten.php">Projecten</a></li>
+                        <!--<li><a href="casestudies.php">Case studies</a></li>-->
+                        <li class="dropdown casestudies">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Case studies <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li class="case-verenigingendatabank"><a href="case-csj.php">Verenigingendatabank</a></li>
+                                <li class="case-kinderopvangleuven"><a href="case-kinderopvangleuven.php">Kinderdagverblijven Leuven</a></li>
+                            </ul>
+                        </li>
+                        <li class="contact"><a href="contact.php">Contacteer ons</a></li>
+                        <!--<li class="sign-up"><a href="signup.html"><span class="white"> Sign Up</span></a></li>-->
+                    </ul>
+                </div><!--/.navbar-collapse -->
+            </div>
+        </div>
+    </div>
+</div>
