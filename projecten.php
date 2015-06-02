7243c71f0274810db4cc830cf55a7f9dd054b13f
jonasdw <jonas@tactics.be>
2015-06-02 14:40:49 +0200

    Clean up projecten -- remove voltooid en in ontwikkeling

--- projecten.php
+++ projecten.php
@@ -25,102 +25,84 @@
   <!--Portfolio Content-->
   <section id="content">
     <div class="container">
-        <h3 class="orange">In uitvoering</h3>
-        <div class="row">
+        <ul class="list-inline portfolio text-center">
+            <li>
+                <div class="thumbs">
+                    <img src="img/pringles_color.gif" class="img-responsive" alt="Image Size 430x280">
+                    <a href="portfolio-pringles.php">
+                    <span class="title">
+                        <span><i class="fa fa-link fa-2x"></i></span>
+                    </span>
+                    </a>
+                </div>
 
-            <ul class="list-inline portfolio text-center">
-                <li>
-                    <div class="thumbs">
-                        <img src="img/pringles_color.gif" class="img-responsive" alt="Image Size 430x280">
-                        <a href="portfolio-pringles.php">
-          				<span class="title">
-          					<span><i class="fa fa-link fa-2x"></i></span>
-          				</span>
-                        </a>
-                    </div>
+                <div class="project-title margin-30">
+                    <h5>Pringles automatisatie</h5>
+                </div>
+            </li>
+            <li>
+                <div class="thumbs">
+                    <img src="img/kinderopvangbrussel.gif" class="img-responsive" alt="Image Size 430x280">
+                    <a href="portfolio-kinderopvangbrussel.php">
+                      <span class="title">
+                          <span><i class="fa fa-link fa-2x"></i></span>
+                      </span>
+                    </a>
+                </div>
 
-                    <div class="project-title margin-30">
-                        <h5>Pringles automatisatie</h5>
-                    </div>
-                </li>
-                <li>
-                    <div class="thumbs">
-                        <img src="img/kinderopvangbrussel.gif" class="img-responsive" alt="Image Size 430x280">
-                        <a href="portfolio-kinderopvangbrussel.php">
-          				<span class="title">
-          					<span><i class="fa fa-link fa-2x"></i></span>
-          				</span>
-                        </a>
-                    </div>
+                <div class="project-title margin-30">
+                    <h5>Kinderopvang in Brussel</h5>
+                </div>
+            </li>
+            <li>
+                <div class="thumbs">
+                    <img src="img/kinderopvangoostende.gif" class="img-responsive" alt="Image Size 430x280">
+                    <a href="portfolio-kinderopvangoostende.php">
+                    <span class="title">
+                        <span><i class="fa fa-link fa-2x"></i></span>
+                    </span>
+                    </a>
+                </div>
 
-                    <div class="project-title margin-30">
-                        <h5>Kinderopvang in Brussel</h5>
-                    </div>
-                </li>
-                <li>
-                    <div class="thumbs">
-                        <img src="img/kinderopvangoostende.gif" class="img-responsive" alt="Image Size 430x280">
-                        <a href="portfolio-kinderopvangoostende.php">
-          				<span class="title">
-          					<span><i class="fa fa-link fa-2x"></i></span>
-          				</span>
-                        </a>
-                    </div>
+                <div class="project-title margin-30">
+                    <h5>Kinderopvang stad Oostende </h5>
+                </div>
+            </li>
+            <li>
+                <div class="thumbs">
+                <img src="img/portfolio1.gif" class="img-responsive" alt="Image Size 430x280">
+                <a href="portfolio-csj.php">
+                    <span class="title">
+                        <span><i class="fa fa-link fa-2x"></i></span>
+                    </span>
+                </a>
+                </div>
 
-                    <div class="project-title margin-30">
-                        <h5>Kinderopvang stad Oostende </h5>
-                    </div>
-                </li>
-                </ul>
-            </div>
-        <h3 class="orange">Voltooid</h3>
-      <div class="row">
-          <ul class="list-inline portfolio text-center">
-            <!--Project Thumbnail-->
+                <div class="project-title margin-30">
+                <h5>Verenigingendatabank stad Antwerpen</h5>
+              </div>
+            </li>
+            <li>
+                <div class="thumbs">
+                <img src="img/portfolio2.gif" class="img-responsive" alt="Image Size 430x280">
+                <a href="portfolio-sportdienst.php">
+                    <span class="title">
+                        <span><i class="fa fa-link fa-2x"></i></span>
+                    </span>
+                </a>
+                </div>
 
-
-        		<li>
-        			<div class="thumbs">
-          			<img src="img/portfolio1.gif" class="img-responsive" alt="Image Size 430x280">
-          			<a href="portfolio-csj.php">
-          				<span class="title">
-          					<span><i class="fa fa-link fa-2x"></i></span>
-          				</span>
-          			</a>
-        			</div>
-
-        			<div class="project-title margin-30">
-        		    <h5>Verenigingendatabank stad Antwerpen</h5>
-        		  </div>
-        		</li>
-
-        		<!--Project Thumbnail-->
-        		<li>
-        			<div class="thumbs">
-          			<img src="img/portfolio2.gif" class="img-responsive" alt="Image Size 430x280">
-          			<a href="portfolio-sportdienst.php">
-          				<span class="title">
-          					<span><i class="fa fa-link fa-2x"></i></span>
-          				</span>
-          			</a>
-        			</div>
-
-        			<div class="project-title margin-30">
-        		    <h5>Sportdienst stad Antwerpen</h5>
-        		  </div>
-        		</li>
-
-        		<!--Project Thumbnail-->
-
-
-
+                <div class="project-title margin-30">
+                <h5>Sportdienst stad Antwerpen</h5>
+              </div>
+            </li>
             <li>
               <div class="thumbs">
                 <img src="img/portfolio5.gif" class="img-responsive" alt="Image Size 430x280">
                 <a href="portfolio-leuven.php">
-          				<span class="title">
-          					<span><i class="fa fa-link fa-2x"></i></span>
-          				</span>
+                        <span class="title">
+                            <span><i class="fa fa-link fa-2x"></i></span>
+                        </span>
                 </a>
               </div>
 
@@ -128,85 +110,67 @@
                 <h5>Kinderopvang stad Leuven </h5>
               </div>
             </li>
+            <li>
+              <div class="thumbs">
+                  <img src="img/kinderopvangantwerpen.gif" class="img-responsive" alt="Image Size 430x280">
+                  <a href="portfolio-kinderopvangantwerpen.php">
+                    <span class="title">
+                        <span><i class="fa fa-link fa-2x"></i></span>
+                    </span>
+                  </a>
+              </div>
 
-              <li>
-                  <div class="thumbs">
-                      <img src="img/kinderopvangantwerpen.gif" class="img-responsive" alt="Image Size 430x280">
-                      <a href="portfolio-kinderopvangantwerpen.php">
-          				<span class="title">
-          					<span><i class="fa fa-link fa-2x"></i></span>
-          				</span>
-                      </a>
-                  </div>
+              <div class="project-title margin-30">
+                  <h5>Kinderopvang stad Antwerpen </h5>
+              </div>
+            </li>
 
-                  <div class="project-title margin-30">
-                      <h5>Kinderopvang stad Antwerpen </h5>
-                  </div>
-              </li>
+          <li>
+              <div class="thumbs">
+                  <img src="img/kinderopvanggent.gif" class="img-responsive" alt="Image Size 430x280">
+                  <a href="portfolio-kinderopvanggent.php">
+                    <span class="title">
+                        <span><i class="fa fa-link fa-2x"></i></span>
+                    </span>
+                  </a>
+              </div>
 
-              <li>
-                  <div class="thumbs">
-                      <img src="img/kinderopvanggent.gif" class="img-responsive" alt="Image Size 430x280">
-                      <a href="portfolio-kinderopvanggent.php">
-          				<span class="title">
-          					<span><i class="fa fa-link fa-2x"></i></span>
-          				</span>
-                      </a>
-                  </div>
+              <div class="project-title margin-30">
+                  <h5>Kinderopvang stad Gent </h5>
+              </div>
+          </li>
+          <li>
+              <div class="thumbs">
+                  <img src="img/portfolio3.gif" class="img-responsive" alt="Image Size 430x280">
+                  <a href="portfolio-kdv-kavka-jespo.php">
+                    <span class="title">
+                        <span><i class="fa fa-link fa-2x"></i></span>
+                    </span>
+                  </a>
+              </div>
 
-                  <div class="project-title margin-30">
-                      <h5>Kinderopvang stad Gent </h5>
-                  </div>
-              </li>
+              <div class="project-title margin-30">
+                  <h5>Kindervreugd - Kavka - Jespo</h5>
+              </div>
+          </li>
+          <li>
+              <div class="thumbs">
+                  <img src="img/portfolio4.gif" class="img-responsive" alt="Image Size 430x280">
+                  <a href="portfolio-hivset.php">
+                    <span class="title">
+                        <span><i class="fa fa-link fa-2x"></i></span>
+                    </span>
+                  </a>
+              </div>
 
-              <li>
-                  <div class="thumbs">
-                      <img src="img/portfolio3.gif" class="img-responsive" alt="Image Size 430x280">
-                      <a href="portfolio-kdv-kavka-jespo.php">
-          				<span class="title">
-          					<span><i class="fa fa-link fa-2x"></i></span>
-          				</span>
-                      </a>
-                  </div>
-
-                  <div class="project-title margin-30">
-                      <h5>Kindervreugd - Kavka - Jespo</h5>
-                  </div>
-              </li>
-
-              <li>
-                  <div class="thumbs">
-                      <img src="img/portfolio4.gif" class="img-responsive" alt="Image Size 430x280">
-                      <a href="portfolio-hivset.php">
-          				<span class="title">
-          					<span><i class="fa fa-link fa-2x"></i></span>
-          				</span>
-                      </a>
-                  </div>
-
-                  <div class="project-title margin-30">
-                      <h5>Vormingscentrum HIVSET</h5>
-                  </div>
-              </li>
-          </ul><!--End Work List-->
+              <div class="project-title margin-30">
+                  <h5>Vormingscentrum HIVSET</h5>
+              </div>
+          </li>
+      </ul><!--End Work List-->
       </div><!--End Row-->
-            
     </div>
   </section>
-  
-    
-  
-  <!--Message-->
-  <!--<section id="message">-->
-    <!--<div class="container">-->
-      <!--<div class="row">-->
-        <!--<div class="col-md-10 col-md-offset-1 text-center">-->
-          <!--<h4 class="gray">Toekomstige projecten</h4>-->
-          <!--<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>-->
-        <!--</div>-->
-      <!--</div>-->
-    <!--</div>-->
-  <!--</section>-->
 
     <!--Bottom Section-->
     <?php
@@ -219,5 +183,5 @@
     <?php
         include('scripts.php');
     ?>
-    </body>
+</body>
 </html>
