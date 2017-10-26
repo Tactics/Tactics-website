416ef0d18a2fdfabc8187ea16e84bf3896f4b7aa
Jonas De Wint <jonas.dewint@gmail.com>
2017-10-26 15:54:56 +0200

    add project images for Oostende

--- portfolio-kinderopvangoostende.php
+++ portfolio-kinderopvangoostende.php
@@ -1,83 +1,95 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php
-    $PageTitle="Tactics bvba | Kinderopvang Oostende";
-    include('header.php');
-    ?>
+  <?php
+  $PageTitle = "Tactics bvba | Kinderopvang Oostende";
+  include('header.php');
+  ?>
 </head>
 <body class="projecten">
 <?php
 include('menu.php');
 ?>
 
-  <!--Page Title-->
-  <section id="page-title">
+<!--Page Title-->
+<section id="page-title">
     <div class="container">
-      <div class="row">
-        <div class="col-md-12 title-orange">
-          <h2>Kinderopvang stad Oostende</h2>
+        <div class="row">
+            <div class="col-md-12 title-orange">
+                <h2>Kinderopvang stad Oostende</h2>
+            </div>
         </div>
-      </div>
     </div>
-  </section>
-  
-  
-  <!--About Content-->
-  <section id="orange-container">
-    <div class="container">
-      <div class="row">
-        
-        <div class="col-md-12">
-          <p class="white">
-              Voor de stad Oostende ontwikkelden we een centraal aanmeldingsregister voor alle opvangvragen voor voorschoolse opvang.<br/>
-              Een aantal modules die werden voorzien ter ondersteuning van deze dienst:<br/>
-            <ul class="white">
-                <li>Aanmeldplatform</li>
-                <li>Planningsysteem</li>
-                <li>Aanwezighedenregistratie</li>
-                <li>Facturatie</li>
-                <li>Communicatie</li>
-            </ul>
-          </p>
-        </div>
-      </div>
-    </div>
-  </section>
-  
-  
-  <!--Team Section-->
-  <section id="content">
-    <div class="container">
-      <div class="row">
+</section>
 
-        <div class="col-md-10">
-          <div class="col-md-10">
-            <h2 class="dark-gray">Links</h2>
-              <ul>
-                  <li>
-                      <a class="content-link" href="https://kinderopvang.oostende.be/" target="_blank">
-                          Kinderopvang oostende
-                      </a>
-                  </li>
-              </ul>
-          </div>
+
+<!--About Content-->
+<section id="orange-container">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-12">
+                <p class="white">
+                    Voor de stad Oostende ontwikkelden we een centraal aanmeldingsregister voor alle opvangvragen voor
+                    voorschoolse opvang.<br/>
+                    Een aantal modules die werden voorzien ter ondersteuning van deze dienst:<br/>
+                <ul class="white">
+                    <li>Aanmeldplatform</li>
+                    <li>Planningsysteem</li>
+                    <li>Aanwezighedenregistratie</li>
+                    <li>Facturatie</li>
+                    <li>Communicatie</li>
+                </ul>
+                </p>
+            </div>
         </div>
-      </div>
     </div>
-  </section>
+</section>
+
+<section id="content">
+    <div class="container">
+        <div class="bxslider">
+            <div><img src="img/Oostende_1.jpg"></div>
+            <div><img src="img/Oostende_2.jpg"></div>
+            <div><img src="img/Oostende_3.jpg"></div>
+        </div>
+        <div class="row">
+            <div class="col-md-10">
+                <div class="col-md-10">
+                    <h2 class="dark-gray">Links</h2>
+                    <ul>
+                        <li>
+                            <a class="content-link" href="https://kinderopvang.oostende.be/" target="_blank">
+                                Kinderopvang oostende
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <!--Bottom Section-->
 <?php
-    include('footer.php');
+include('footer.php');
 ?>
 
-    
-    <!-- Javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <?php
-     include('scripts.php');
-    ?>
-    </body>
+
+<!-- Javascript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<?php
+include('scripts.php');
+?>
+<script>
+    $(function () {
+        $('.bxslider').bxSlider({
+            mode: 'fade',
+            captions: true,
+            slideWidth: 600
+        });
+    });
+</script>
+</body>
 </html>
